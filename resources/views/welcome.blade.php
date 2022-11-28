4bc0195ef522e6fbd3b6a68ce4a9b91e46a958b8
Valtersystem <valtersantos.ifsp@gmail.com>
2022-11-28 23:14:30 +0000

    Finalizando o Menu de navegação.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,13 @@
-<!DOCTYPE html>
+<x-app-layout>
+
+    Hola Mundo
+
+</x-app-layout>
+
+
+
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +138,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
