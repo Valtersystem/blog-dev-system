48188aa76e3150bd1de0ffcf59b12ebfd500077b
Valtersystem <valtersantos.ifsp@gmail.com>
2023-01-19 22:12:00 +0000

    .

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\UserController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
