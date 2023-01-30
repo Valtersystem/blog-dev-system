3b92e85db1158f9a279b6f9388ecfdc1bd674ad9
Valtersystem <valtersantos.ifsp@gmail.com>
2023-01-30 23:45:59 +0000

    ..

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -27,19 +27,19 @@
         </article> --}}
 
 
-        <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-96">
-            <article class="block w-full h-full">
+        <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-full w-60 md:w-96">
+           
                 @if ($post->image)
                 <img alt="blog photo" src="{{$post->image->url}}" class="object-cover w-full max-h-40"/>
                 @else
-                <img alt="blog photo" src="https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_960_720.jpg" class="object-cover w-full max-h-40"/>
+                <img alt="blog photo" src="https://cdn.pixabay.com/photo/2014/12/27/15/40/office-581131_960_720.jpg" class="object-cover w-full max-h-44"/>
                 @endif
 
-                <div class="w-full p-4 bg-white dark:bg-gray-800">
+                <div class="w-full p-4 bg-white dark:bg-gray-800 h-full">
                     <p class="font-medium text-indigo-500 text-md">
                         Post Date: {{$post->created_at->toFormattedDateString()}}
                     </p>
-                    <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white">
+                    <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white h-full flex flex-col justify-around">
                         <a href="{{route('posts.show', $post)}}">
                             {{$post->name}}
                         </a>
@@ -47,7 +47,7 @@
                     {{-- <p class="font-light text-gray-400 dark:text-gray-300 text-md">
                         {!!$post->extract!!}
                     </p> --}}
-                    <div class="flex flex-wrap items-center mt-4 justify-starts">
+                    <div class="flex flex-wrap items-center mt-4 justify-starts gap-3">
                         @foreach ($post->tags as $tag)
                         <div class="text-xs mr-2 py-1.5 px-4 text-white {{$tag->color}} rounded-2xl">
                             <a href="{{route('posts.tag', $tag)}}">
@@ -57,7 +57,7 @@
                         @endforeach
                     </div>
                 </div>
-            </article>
+           
         </div>
 
 
